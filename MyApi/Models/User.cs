06bebdf04c60dd8c635dc1484d2dc731f2c39d28
joe <tamatenoworries@gmail.com>
2020-03-17 15:07:26 +1030

    * .gitignore: ini
    * MyApi.sln:
    * Program.cs:
    * Startup.cs:
    * MyApi.csproj:
    * User.cs:
    * appsettings.json:
    * WeatherForecast.cs:
    * UserContext.cs:
    * appsettings.Development.json:
    * UsersController.cs:
    * launchSettings.json:
    * WeatherForecastController.cs:

---
+++ MyApi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MyApi.Models
+{
+    public class User
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
---
+++ MyApi/Models/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace MyApi.Models
+{
+    public class UserContext:DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options): base(options)
+        {
+        }
+        public DbSet<User> TodoItems { get; set; }
+    }
+}
